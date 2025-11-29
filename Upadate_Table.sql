d2fabf7b571f13289b145eb41d27568d5f27a8ef
Aloniih <aloniihfirmino2010@gmail.com>
2025-11-29 19:47:27 -0300

    Alterando alguns dados existentes

---
+++ Upadate_Table.sql
@@ -0,0 +1,19 @@
+--------Modificar um nome------------
+
+UPDATE Aluno
+SET nome = 'Roberto Carlos'
+WHERE id_Aluno = 1;
+
+--------Modificar um número----------
+
+UPDATE Professor
+SET telefone = '+5516999244421'
+WHERE Id_Professor = 1;
+ 
+--------Modificar uma Disciplina ----
+UPDATE Disciplina
+SET Nome_Disciplina = 'Língua Portuguesa'
+WHERE Id_Disciplina = 3;
+
+------------------------------------
+
